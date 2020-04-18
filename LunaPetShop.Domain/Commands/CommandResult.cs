2bbf4ce6e511bb19aead1425cb4ce77f8cfa3bbc
Laís Rosa <msilva@brasilseg.com.br>
2020-04-18 13:43:37 -0300

    Remove imports from command result

--- LunaPetShop.Domain/Commands/CommandResult.cs
+++ LunaPetShop.Domain/Commands/CommandResult.cs
@@ -1,8 +1,5 @@
-using System.Collections.Generic;
 using Flunt.Notifications;
-using Flunt.Validations;
 using LunaPetShop.Domain.Commands.Contracts;
-using LunaPetShop.Domain.Entities;
 
 namespace LunaPetShop.Domain.Commands
 {
