fd72210a544401911c2ac0e6891fdd416df5f381
Laís Rosa <msilva@brasilseg.com.br>
2020-04-18 22:02:34 -0300

    Respository

---
+++ LunaPetShop.Domain.Infra/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+using System;
+using LunaPetShop.Domain.Repository;
+
+namespace LunaPetShop.Domain.Infra.Respositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public void addUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void deleteUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void getById(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateUser(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- LunaPetShop.Domain/Respositories/IUserRepository.cs
+++ LunaPetShop.Domain/Respositories/IUserRepository.cs
@@ -4,6 +4,7 @@ namespace LunaPetShop.Domain.Repository
 {
     public interface IUserRepository
     {
+        void getById(Guid Id);
         void addUser();
         void deleteUser();
         void updateUser(Guid Id);
