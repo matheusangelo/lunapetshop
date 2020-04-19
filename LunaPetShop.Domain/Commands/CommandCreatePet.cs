954e23c3b315d919bbee0fa4c4755307b8109517
Laís Rosa <msilva@brasilseg.com.br>
2020-04-19 12:56:00 -0300

    creating command

---
+++ LunaPetShop.Domain/Commands/CommandCreatePet.cs
@@ -0,0 +1,25 @@
+using Flunt.Notifications;
+using Flunt.Validations;
+using LunaPetShop.Domain.Commands.Contracts;
+
+namespace LunaPetShop.Domain.Commands
+{
+    public class CommandCreatePet : Notifiable, ICommand
+    {
+        public string Name { get; set; }
+        public double Weigth { get; set; }
+        public int Age { get; set; }
+        public string Sex { get; set; }
+        public string Breed { get; set; }
+        public bool Castrated { get; set; }
+        public double Size { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(
+                    new Contract()
+                    .HasMinLen(Name, 3, "Name", "Name should have at least 3 chars")
+                );
+        }
+    }
+}
