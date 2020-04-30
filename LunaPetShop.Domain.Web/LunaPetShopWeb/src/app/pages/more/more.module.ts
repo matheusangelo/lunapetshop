d7a992d657552b6a704fc075de71433561a475a3
Laís Rosa <msilva@brasilseg.com.br>
2020-04-30 20:26:01 -0300

    creating modules of pages

---
+++ LunaPetShop.Domain.Web/LunaPetShopWeb/src/app/pages/more/more.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MoreComponent } from './more.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MoreComponent]
+})
+export class MoreModule { }
